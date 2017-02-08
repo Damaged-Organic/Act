cd54f5aef34a85c068ff15fbf2bad5ce3bd46b29
Kid Binary <kid.binary@gmail.com>
2017-02-08 14:12:25 +0200

    Completed scraping middleware

--- act/act/services/crawler_detectors.py
+++ act/act/services/crawler_detectors.py
@@ -4,14 +4,15 @@ class CrawlerDetector:
 
     _user_agents = []
 
-    def is_user_agent_matching(self, request):
-        request_user_agent = request.META.get(self.HTTP_USER_AGENT, None)
+    @classmethod
+    def is_user_agent_matching(cls, request):
+        request_user_agent = request.META.get(cls.HTTP_USER_AGENT, None)
 
         if request_user_agent:
             request_user_agent = request_user_agent.lower()
 
         matching = next((
-            user_agent for user_agent in self._user_agents
+            user_agent for user_agent in cls._user_agents
             if user_agent in request_user_agent),
             None)
 
--- act/website/middleware/social_crawlers_middleware.py
+++ act/website/middleware/social_crawlers_middleware.py
@@ -1,6 +1,7 @@
 # act/website/middleware/social_crawlers_middleware.py
 from django.shortcuts import get_object_or_404
 from django.template.loader import render_to_string
+from django.utils.html import format_html
 
 from act.services.crawler_detectors import (
     FacebookCrawlerDetector, VkontakteCrawlerDetector, TwitterCrawlerDetector,
@@ -10,27 +11,25 @@ from ..models import Scraping
 
 
 class SocialCrawlersMiddleware(object):
+    crawler_detectors = (
+        FacebookCrawlerDetector,
+        VkontakteCrawlerDetector,
+        TwitterCrawlerDetector,
+    )
+
     def process_response(self, request, response):
         if self.is_crawler(request):
             scraped = get_object_or_404(Scraping, path=request.path)
 
             response.content = render_to_string(
                 'website/middleware/social_crawlers.html',
-                {'head': scraped.head})
+                {'head': format_html(scraped.head)})
 
         return response
 
     def is_crawler(self, request):
-        facebook_crawler_detector = FacebookCrawlerDetector()
-        if facebook_crawler_detector.is_user_agent_matching(request):
-            return True
-
-        vkontakte_crawler_detector = VkontakteCrawlerDetector()
-        if vkontakte_crawler_detector.is_user_agent_matching(request):
-            return True
-
-        twitter_crawler_detector = TwitterCrawlerDetector()
-        if twitter_crawler_detector.is_user_agent_matching(request):
-            return True
+        for crawler_detector in self.crawler_detectors:
+            if crawler_detector.is_user_agent_matching(request):
+                return True
 
         return False
